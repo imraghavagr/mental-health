85f44be12600ebe81ec098ea534bf8061ed5a4d6
IndraP24 <indrashis985@gmail.com>
2022-04-14 19:48:07 +0530

    Update donwload script

--- scripts/data/download.py
+++ scripts/data/download.py
@@ -1,3 +1,16 @@
 import requests
 import click
 
+
+@click.command()
+@click.argument('url')
+@click.argument('filename', type=click.Path())
+def download_file(url, filename):
+    """Downloads a file from a URL to a local file."""
+    print('Downloading {} to {}...'.format(url, filename))
+    response = requests.get(url, stream=True)
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+
+if __name__ == '__main__':
+    download_file()
