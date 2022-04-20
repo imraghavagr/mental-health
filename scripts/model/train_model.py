aac6651c716f9cbd7d7fd9513d50678e8bfcfc06
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-04-20 23:45:37 +0530

    modify train_model.py

---
+++ scripts/model/train_model.py
@@ -0,0 +1,29 @@
+import click
+import pandas as pd
+from pycaret.classification import *
+
+@click.command()
+@click.argument('data_path', type=click.Path())
+@click.argument('target_path', type=click.Path())
+def train_model(data_path,target_path):
+    """ Read the final processed data, train the optimal model and save the pkl file."""
+    target_path = target_path + 'finalRFmodel2_20APR2022'
+    data_path = data_path + 'processed_data.csv'
+
+
+    df = pd.read_csv(data_path)
+    print("Reading processed data - Successful")
+
+    s = setup(df, target = 'mental_disorder')
+    print("Setting up pycaret env - Successful")
+
+    rf = create_model("rf")
+    print("Creating Random Forest model - Successful")
+
+    save_model(rf,target_path)
+    print(f"Successfuly save model pickel file to {target_path}")
+
+
+
+if __name__ == "__main__":
+    train_model()
